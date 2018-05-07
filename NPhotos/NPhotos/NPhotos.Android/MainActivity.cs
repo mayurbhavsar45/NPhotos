0d85b854b4a99004776a0193f4aa7b2a2906f0d8
Mayurbhavsar <mayurbhavsar45@gmail.com>
2018-05-07 18:00:00 +0530

    Added Login Screen
    Created UI for Login Screen.

--- NPhotos/NPhotos/NPhotos.Android/MainActivity.cs
+++ NPhotos/NPhotos/NPhotos.Android/MainActivity.cs
@@ -1,23 +1,34 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Widget;
 using Prism;
 using Prism.Ioc;
-
 namespace NPhotos.Droid
 {
-    [Activity(Label = "NPhotos", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "NPhotos", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        App _app;
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
-
+           
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App(new AndroidInitializer()));
+            _app = new App();
+            Window.SetStatusBarColor(this.Resources.GetColor(Resource.Color.appMajorColor));
+        }
+
+        public override void OnBackPressed()
+        {
+            if (_app.DoBack())
+            {
+                base.OnBackPressed();
+            }
         }
     }
 
--- NPhotos/NPhotos/NPhotos/App.xaml.cs
+++ NPhotos/NPhotos/NPhotos/App.xaml.cs
@@ -12,10 +12,10 @@ namespace NPhotos
     public partial class App : PrismApplication
     {
         /* 
-         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
-         * This imposes a limitation in which the App class must have a default constructor. 
-         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
-         */
+* The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+* This imposes a limitation in which the App class must have a default constructor. 
+* App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+*/
         public App() : this(null) { }
 
         public App(IPlatformInitializer initializer) : base(initializer) { }
@@ -24,13 +24,25 @@ namespace NPhotos
         {
             InitializeComponent();
 
-            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+            await NavigationService.NavigateAsync("NavigationPage/LoginPage");
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterForNavigation<NavigationPage>();
-            containerRegistry.RegisterForNavigation<MainPage>();
+            containerRegistry.RegisterForNavigation<LoginPage>();
+            containerRegistry.RegisterForNavigation<HomePage>();
+        }
+
+        public bool DoBack()
+        {          
+            NavigationPage mainPage = MainPage as NavigationPage;
+            if (mainPage != null)
+            {
+                return mainPage.Navigation.NavigationStack.Count > 1;
+            }
+            return true;
+            
         }
     }
 }
